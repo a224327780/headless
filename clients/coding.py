3ace7e27bd032bd9a551aac8a747f0091c6a6897
caoyufei <atcaoyufei@gmail.com>
2021-06-01 20:41:18 +0800

    1.04

---
+++ clients/coding.py
@@ -0,0 +1,32 @@
+import asyncio
+import json
+
+import requests
+
+from libs.base import BaseClient
+
+
+class Coding(BaseClient):
+
+    def __init__(self):
+        super().__init__()
+        self.url = 'https://e.coding.net/login'
+
+    async def handler(self, **kwargs):
+        self.logger.info(f'{self.username} start login.')
+        await asyncio.sleep(8)
+        await self.page.type('#account', self.username, {'delay': 30})
+        await asyncio.sleep(0.5)
+        await self.page.type('#password', self.password, {'delay': 30})
+        await asyncio.sleep(0.5)
+        await self.page.click('div.cuk-checkbox-2-box')
+        await asyncio.sleep(0.5)
+        await self.page.click('button[type="Submit"]')
+        await asyncio.sleep(8)
+
+        cookies = await self.get_cookies()
+        cookies = json.dumps(cookies)
+        self.logger.info(self.page.url)
+        result = requests.post(f'{self.api}/coding/save', data={'cookies': cookies}).text
+        self.logger.info(result)
+
