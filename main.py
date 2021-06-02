cbb19aab888f2fc60277d2c9767580dbab94060c
caoyufei <atcaoyufei@gmail.com>
2021-06-02 11:39:09 +0800

    1.042

--- main.py
+++ main.py
@@ -20,10 +20,7 @@ def script_main(params):
                 loop.run_until_complete(func(**params))
             except Exception as e:
                 logging.warning(e)
-                exit(1)
-            finally:
-                loop.close()
-                exit(0)
+            break
 
 
 def main():
