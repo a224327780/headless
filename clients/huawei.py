054aeb07ba3333d0fb53378aeea287c25d0f0bb2
caoyufei <atcaoyufei@gmail.com>
2021-07-21 16:06:18 +0800

    1.11

--- clients/huawei.py
+++ clients/huawei.py
@@ -31,13 +31,13 @@ class HuaWei(BaseHuaWei):
         await self.sign_task()
 
         await self.async_timeout_run('delete_function')
+        await self.async_timeout_run('delete_api')
+        await self.async_timeout_run('delete_api_group')
 
         if await self.new_project():
             await self.start()
 
         await self.async_timeout_run('delete_project')
-        await self.async_timeout_run('delete_api')
-        await self.async_timeout_run('delete_api_group')
 
         return await self.get_credit()
 
